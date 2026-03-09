dd67a32c5169d9612c3e0859bcaf534e24ff2d17
Hamza Almughrabi <hmughrabi2001@gmail.com>
2026-03-09 11:28:12 +0300

    Update ddl_silver_SQL.sql

--- scripts/silver/ddl_silver_SQL.sql
+++ scripts/silver/ddl_silver_SQL.sql
@@ -1,1 +1,34 @@
 
+
+
+
+IF OBJECT_ID ('silver.crm_prd_info', 'U') IS NOT NULL
+	DROP TABLE silver.crm_prd_info;
+CREATE TABLE silver.crm_prd_info (
+	prd_id			    INT,
+	cat_id			    NVARCHAR(50),
+	prd_key			    NVARCHAR(50),
+	prd_nm			    NVARCHAR(50),
+	prd_cost		    INT,
+	prd_line		    NVARCHAR(50),
+	prd_start_date  DATE,
+	prd_end_date	  DATE,
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+GO
+
+IF OBJECT_ID ('silver.crm_sls_details', 'U') IS NOT NULL
+DROP TABLE silver.crm_sls_details;
+
+CREATE TABLE silver.crm_sls_details (
+	sls_ord_num	 	 NVARCHAR(50),
+	sls_prd_key		 NVARCHAR(50),
+	sls_cust_id	 	 INT,
+	sls_order_dt   DATE,
+	sls_ship_dt		 DATE,
+	sls_due_dt	 	 DATE,
+	sls_sales		   INT,
+	sls_quantity	 INT,
+	sls_price		   INT,
+	wh_create_date DATETIME2 DEFAULT GETDATE()
+);
