3cf76b0f79630689b51f097aa790d1b8c4d10ec8
Hamza Almughrabi <hmughrabi2001@gmail.com>
2026-03-09 11:39:23 +0300

    Update ddl_silver_SQL.sql

--- scripts/silver/ddl_silver_SQL.sql
+++ scripts/silver/ddl_silver_SQL.sql
@@ -27,22 +27,22 @@ CREATE TABLE bronze.crm_cust_info (
     cst_marital_status NVARCHAR(50),
     cst_gndr           NVARCHAR(50), 
     cst_create_date    DATE
-	dwh_create_date DATETIME2 DEFAULT GETDATE()
+	dwh_create_date    DATETIME2 DEFAULT GETDATE()
 );
 GO
 
 IF OBJECT_ID ('silver.crm_prd_info', 'U') IS NOT NULL
 	DROP TABLE silver.crm_prd_info;
 CREATE TABLE silver.crm_prd_info (
-	prd_id			    INT,
-	cat_id			    NVARCHAR(50),
-	prd_key			    NVARCHAR(50),
-	prd_nm			    NVARCHAR(50),
-	prd_cost		    INT,
-	prd_line		    NVARCHAR(50),
-	prd_start_date  DATE,
+	prd_id			  INT,
+	cat_id			  NVARCHAR(50),
+	prd_key			  NVARCHAR(50),
+	prd_nm			  NVARCHAR(50),
+	prd_cost		  INT,
+	prd_line		  NVARCHAR(50),
+	prd_start_date    DATE,
 	prd_end_date	  DATE,
-	dwh_create_date DATETIME2 DEFAULT GETDATE()
+	dwh_create_date   DATETIME2 DEFAULT GETDATE()
 );
 GO
 
@@ -59,7 +59,7 @@ CREATE TABLE silver.crm_sls_details (
 	sls_sales		INT,
 	sls_quantity	INT,
 	sls_price		INT,
-	wh_create_date DATETIME2 DEFAULT GETDATE()
+	wh_create_date  DATETIME2 DEFAULT GETDATE()
 );
 GO
 
